13910c256218bdfd82ac24c3679f29b9c99fc2c7
rivnoj <84639053+rivnoj@users.noreply.github.com>
2024-03-20 11:09:00 -0300

    [LIESR.19] CPL2.2 - Usando Jakarta Persistence

--- src/main/java/com/rivnoj/awpag/api/controller/ClienteController.java
+++ src/main/java/com/rivnoj/awpag/api/controller/ClienteController.java
@@ -1,6 +1,5 @@
 package com.rivnoj.awpag.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,22 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.rivnoj.awpag.domain.model.Cliente;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
 @RestController
 public class ClienteController {
+
+  @PersistenceContext
+  private EntityManager manager;
+
   @GetMapping("/clientes")
   public List<Cliente> listar() {
-    Cliente cliente1 = new Cliente();
-    cliente1.setId(1L);
-    cliente1.setNome("João");
-    cliente1.setTelefone("81 97654-3876");
-    cliente1.setEmail("joaodascouves@email.com");
-
-    Cliente cliente2 = new Cliente();
-    cliente2.setId(2L);
-    cliente2.setNome("Marcelo");
-    cliente2.setTelefone("81 93444-3476");
-    cliente2.setEmail("marcelosilva@email.com");
-
-    return Arrays.asList(cliente1, cliente2);
+    return manager.createQuery("from Cliente", Cliente.class)
+            .getResultList();
   }
 }
--- src/main/java/com/rivnoj/awpag/domain/model/Cliente.java
+++ src/main/java/com/rivnoj/awpag/domain/model/Cliente.java
@@ -1,13 +1,29 @@
 package com.rivnoj.awpag.domain.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
+//@Table(name = "tb_cliente")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
 public class Cliente {
+  @EqualsAndHashCode.Include
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
   private String nome;
+
   private String email;
+
+  @Column(name = "fone")
   private String telefone;
 }
