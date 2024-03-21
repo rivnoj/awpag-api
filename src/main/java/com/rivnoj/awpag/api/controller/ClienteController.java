1f8b3ba62503b175a206545a728d4bfd79b60c88
Rivaldo de Oliveira <rivnoj@gmail.com>
2024-03-21 11:07:07 -0300

    [LIESR.19] CPL3.2 - Boas práticas para Exception Handler

--- src/main/java/com/rivnoj/awpag/api/controller/ClienteController.java
+++ src/main/java/com/rivnoj/awpag/api/controller/ClienteController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rivnoj.awpag.domain.exception.NegocioException;
 import com.rivnoj.awpag.domain.model.Cliente;
 import com.rivnoj.awpag.domain.repository.ClienteRepository;
 import com.rivnoj.awpag.domain.service.CadastroClienteService;
@@ -91,10 +89,5 @@ public class ClienteController {
     cadastroClienteService.excluir(clienteId);
 
     return ResponseEntity.noContent().build();
-  }
-
-  @ExceptionHandler(NegocioException.class)
-  public ResponseEntity<String> capturar(NegocioException e) {
-    return ResponseEntity.badRequest().body(e.getMessage());
   }
 }
--- src/main/java/com/rivnoj/awpag/api/controller/ParcelamentoController.java
+++ src/main/java/com/rivnoj/awpag/api/controller/ParcelamentoController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rivnoj.awpag.domain.exception.NegocioException;
 import com.rivnoj.awpag.domain.model.Parcelamento;
 import com.rivnoj.awpag.domain.repository.ParcelamentoRepository;
 import com.rivnoj.awpag.domain.service.ParcelamentoService;
@@ -45,10 +43,5 @@ public class ParcelamentoController {
   @ResponseStatus(HttpStatus.CREATED)
   public Parcelamento cadastrar(@Valid @RequestBody Parcelamento parcelamento) {
     return parcelamentoService.cadastrar(parcelamento);
-  }
-
-  @ExceptionHandler(NegocioException.class)
-  public ResponseEntity<String> capturar(NegocioException e) {
-    return ResponseEntity.badRequest().body(e.getMessage());
   }
 }
